9f27449762554683120fe961c9e37c7b4c098ff3
Adarsh-g-s <adarsh431593@gmail.com>
2017-06-21 09:54:24 +0200

    Changes unwanted

--- Columns.java
+++ Columns.java
@@ -55,7 +55,7 @@ public class Columns {
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch())
 				display.sleep();
-			  System.out.println("Test Buddy");
+			  System.out.println("Test Buddy1");
 		}
 		display.dispose();
 	}
